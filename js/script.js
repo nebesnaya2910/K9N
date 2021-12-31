b1e28eb98c33c2ec39877f148839a11bab345fc7
nebesnaya2910 <nebesnayaa@inbox.ru>
2021-12-31 20:11:11 +0300

    test

---
+++ js/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $('.slider').slick({
+        arrows: false,
+        dots: true,
+        infinite: false,
+        speed: 1000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2500,
+        draggable: false,
+        swipe: true,
+        lazyLoad: 'ondemand',
+
+
+    });
+});
